3d03c087b7306855cea38771b0c2e62e8e535148
Prince Boamah <regioths@gmail.com>
2021-10-12 13:38:17 +0000

    Prince: Solved

---
+++ js/DONE/task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const death = 'none';
+const country = 'Ghana';
+const wifeMateria = 'Doris';
+const statusOfMarriage = 1;
+const religion = 'Christian';
