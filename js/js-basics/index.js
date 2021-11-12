5c8ed8e26924b2d7acfb27a17acfd518d8ca92fd
Prince Boamah <regioths@gmail.com>
2021-11-12 13:42:33 +0000

    Prince: Added an index.js

---
+++ js/js-basics/index.js
@@ -0,0 +1,15 @@
+// hello world
+
+console.log('hello, world!')
+
+// variables
+let firstName = "Prince";
+let lastName = "Boamah";
+const DATEOFBIRTH = '21 September, 1996';
+
+console.log(firstName);
+console.log(firstName + ' ' + lastName);
+console.log(DATEOFBIRTH);
+
+
+
