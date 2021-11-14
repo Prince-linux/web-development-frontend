d3501d113c532607a1f07906025ed0b0ee903c4f
Prince Boamah <regioths@gmail.com>
2021-11-14 23:38:44 +0000

    Prince: Added a simple function

--- js/free.js
+++ js/free.js
@@ -2,3 +2,9 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
 console.log(myStr);
 console.log("Hello");
+
+function mySum(num) {
+    return num + 5;
+}
+
+console.log(mySum(10));
