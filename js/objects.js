a95cefa647740686feea357bec945f0f2ec858e9
Prince Boamah <regioths@gmail.com>
2021-11-24 08:07:24 +0000

    Prince:

---
+++ js/objects.js
@@ -0,0 +1,11 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Bobby",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Bone", "Human"]
+  
+    // Only change code above this line
+  };
+
+console.log(myDog);
