759b9ea1efdf7058adea9fc61ec51f254958080c
Prince Boamah <regioths@gmail.com>
2021-12-08 23:41:13 +0000

    Added a recursive function

---
+++ js/recur2.js
@@ -0,0 +1,12 @@
+//Using Recursion to Create a Countdown
+function countdown(n){
+    if (n < 1) {
+      return [];
+    }  else  {
+      const countArray = countdown(n - 1);
+      countArray.unshift(n);
+      return countArray;
+    }
+  }
+
+console.log(countdown(20));
