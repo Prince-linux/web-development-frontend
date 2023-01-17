f5411aa059b8ea2d915a563fd0a6b7092edd3e77
Prince Boamah <regioths@gmail.com>
2023-01-17 20:12:23 +0000

    A basic mathematical operation

--- js/my_Js_playground/counter/index.js
+++ js/my_Js_playground/counter/index.js
@@ -1,1 +1,7 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+let myAge = 10;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
