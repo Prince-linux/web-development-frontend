41f215e7c24ab3bdb0bf5f8e2e002de8e5a9a6ab
Prince Boamah <regioths@gmail.com>
2023-01-19 20:50:11 +0000

    Added my first increment function

--- js/my_Js_playground/counter/index.js
+++ js/my_Js_playground/counter/index.js
@@ -1,10 +1,10 @@
 // document.getElementById("count-el").innerText = 5
 
-let bonusPoints = 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints - 75;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in HTML to reflect the new count.
+
+function increment() {
+    console.log("The button was clicked")
+}
