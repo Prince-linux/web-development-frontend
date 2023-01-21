b30a36daf73c4e05508b1257137744633ad359f4
Prince Boamah <regioths@gmail.com>
2023-01-21 23:49:16 +0000

    Example of a function in js

--- js/my_Js_playground/counter/index.js
+++ js/my_Js_playground/counter/index.js
@@ -5,6 +5,16 @@
 // increment the count variable when the button is clicked
 // change the count-el in HTML to reflect the new count.
 
-function increment() {
-    console.log("The button was clicked")
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+function countdown() {
+    console.log(1)
+    console.log(2)
+    console.log(3)
+    console.log(4)
+    console.log(5)
 }
+
+countdown()
