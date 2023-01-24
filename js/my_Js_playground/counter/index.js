52e3d747d7aca3332769ee444e29143c1d61f830
Prince Boamah <regioths@gmail.com>
2023-01-24 19:17:13 +0000

    Implemented the increment function

--- js/my_Js_playground/counter/index.js
+++ js/my_Js_playground/counter/index.js
@@ -5,9 +5,13 @@
 // increment the count variable when the button is clicked
 // change the count-el in HTML to reflect the new count.
 
-count = 0
+let countEl = document.getElementById('count-el');
+
+
+let count = 0
 
 function increment() {
     count = count + 1
+    countEl.innerText = count
     console.log(count)
 }
