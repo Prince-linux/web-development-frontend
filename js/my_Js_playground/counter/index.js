2b1f8eb104e30a8f2cacdab9337e2f487ebbd21d
Prince Boamah <regioths@gmail.com>
2023-01-25 20:46:58 +0000

    completed app

--- js/my_Js_playground/counter/index.js
+++ js/my_Js_playground/counter/index.js
@@ -1,33 +1,27 @@
-// document.getElementById("count-el").innerText = 5
-
 // initialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in HTML to reflect the new count.
 
-// let countEl = document.getElementById('count-el');
+let countEl = document.getElementById('count-el');
 
+let saveEl = document.getElementById('save-el');
+let count = 0
 
-// let count = 0
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
 
-// function increment() {
-//     count = count + 1
-//     countEl.innerText = count
-//     console.log(count)
-// }
+function save() {
+    let countStr = " " + count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
 
-// function save() {
-//     console.log(count)
-// }
-
-// function reset() {
-//     count = 0
-//     countEl.innerText = count
-// }
-
-let welcomeEl = document.getElementById("welcome-el")
-
-let name = "Prince"
-let greeting = "Welcome"
-
-welcomeEl.innerText = greeting + " " + name + "!"
+function reset() {
+    count = 0
+    countEl.textContent = count
+    saveEl.textContent = "Entries reset!"
+}
